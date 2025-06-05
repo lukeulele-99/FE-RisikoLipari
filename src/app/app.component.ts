24880cb73e197dbed0bc7c7425121df2a1a583d2
DESKTOP-VQQ442N\User <luca.albertario@liparipeople.com>
2025-06-06 01:13:25 +0200

    mappa e navbar di default create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { MapComponent } from "./map/map.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
