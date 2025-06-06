5f6f1da413eee3363dcdba605ba1b9b2bb00d45a
DESKTOP-VQQ442N\User <luca.albertario@liparipeople.com>
2025-06-06 11:16:13 +0200

    aggiunta company.html (da ultimare)
    Generazione componenti di default senza style applicato

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { MapComponent } from "./map/map.component";
+import { CompanyComponent } from "./company/company.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, MapComponent],
+  imports: [RouterOutlet, NavbarComponent, MapComponent, CompanyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../../navbar/navbar.component";
+import { MapComponent } from "../../map/map.component";
+import { CompanyComponent } from "../../company/company.component";
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [NavbarComponent, MapComponent, CompanyComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
