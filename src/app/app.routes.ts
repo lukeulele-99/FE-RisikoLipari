32a95faf9200d64ca93845367e451f44a9e38757
Carol04k <79216467+Carol04k@users.noreply.github.com>
2025-06-06 16:01:15 +0200

    configurazione delle route (da capire bene)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './pages/user/user.component';
 import { GameComponent } from './pages/game/game.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    {path: '', component: UserComponent},
-    {path: 'game', component: GameComponent}
+    {path: '', component: AppComponent},
+    {path: 'game', component: GameComponent},
+    {path: 'user', component: UserComponent}
 ];
