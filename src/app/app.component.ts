467dea2356d8e73fa487cb5424e96434501135f2
Carol04k <79216467+Carol04k@users.noreply.github.com>
2025-06-06 11:53:21 +0200

    aggiunti services/user e user model e component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { MapComponent } from "./map/map.component";
 import { CompanyComponent } from "./company/company.component";
+import { UserComponent } from "./pages/user/user.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, MapComponent, CompanyComponent],
+  imports: [RouterOutlet, NavbarComponent, MapComponent, CompanyComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
