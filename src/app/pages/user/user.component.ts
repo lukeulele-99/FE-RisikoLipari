2cc5dfc4a622d909aaa7f5339f30e0a196619185
Carol04k <79216467+Carol04k@users.noreply.github.com>
2025-06-06 12:25:46 +0200

    aggiunti collegamento a http di user + modifiche a html

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { User } from '../../models/User';
+import { UserService } from '../../services/user/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-user',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  users: User[] = [];
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.usersUpdatedSubject.subscribe((user) => {
+      console.log(user);
+      this.users = user;
+    })
+
+    this.userService.getUsers().subscribe((response) => this.users = response.data);
+  }
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Response } from '../../Response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private apiUrl= 'http://localhost:8080/api/users';
+
+  usersUpdatedSubject = new BehaviorSubject<any>(null);
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<Response> {
+    return this.http.get<Response>(this.apiUrl);
+  }
 }
