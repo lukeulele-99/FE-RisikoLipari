618282bf37a34775638ee54e0b77ecd95472f1b4
Carol04k <79216467+Carol04k@users.noreply.github.com>
2025-06-09 17:51:08 +0200

    .

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -10,7 +10,7 @@ import { EmployeeComponent } from "../../employee/employee.component";
 
 @Component({
   selector: 'app-game',
-  imports: [NavbarComponent, MapComponent, CompanyComponent, CommonModule, EmployeeComponent],
+  imports: [MapComponent, CompanyComponent, CommonModule, EmployeeComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
