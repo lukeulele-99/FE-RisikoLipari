9d74f21ff7c7a7e6a77d0d0b14407995568384cb
DESKTOP-VQQ442N\User <luca.albertario@liparipeople.com>
2025-06-09 12:06:56 +0200

    Creazione tabella di default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { GameComponent } from './pages/game/game.component';
 import { AppComponent } from './app.component';
 import { RulesComponent } from './pages/rules/rules.component';
 import { TutorialComponent } from './pages/tutorial/tutorial.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'user', pathMatch: 'full'},
     {path: 'game', component: GameComponent},
     {path: 'user', component: UserComponent},
     {path: 'rules', component: RulesComponent},
-    {path: 'tutorial', component: TutorialComponent}
+    {path: 'tutorial', component: TutorialComponent},
+    {path: 'employee', component: EmployeeComponent}
 ];
