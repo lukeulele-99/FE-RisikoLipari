7c7d19ecf1de85527a1fd28f1bbf673b8c8d9721
Carol04k <79216467+Carol04k@users.noreply.github.com>
2025-06-09 14:26:35 +0200

    aggiunte company e region components

---
+++ src/app/region/region.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-region',
+  imports: [],
+  templateUrl: './region.component.html',
+  styleUrl: './region.component.css'
+})
+export class RegionComponent {
+
+}
