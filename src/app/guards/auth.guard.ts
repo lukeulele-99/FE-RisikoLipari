59800a61e1e91b6194f7cd80ed52fd0dad46704f
DESKTOP-VQQ442N\User <luca.albertario@liparipeople.com>
2025-06-09 14:37:19 +0200

    Creazione AuthService che lavora in LocalStorage

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,14 +1,16 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const isAuthenticated = localStorage.getItem('auth') === 'true'; //CON LOCAL STORAGE PER ORA
-
+  const authService = inject(AuthService)
   if (isAuthenticated) {
     return true;
   } else {
-    router.navigate(['/user']);         
+    router.navigate(['/user']);
     return false;
   }
 };
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() {}
+
+  login(): void {
+    localStorage.setItem('auth', 'true');
+  }
+
+  logout(): void {
+    localStorage.removeItem('auth');
+  }
+
+  isLoggedIn(): boolean {
+    return localStorage.getItem('auth') === 'true';
+  }
+}
