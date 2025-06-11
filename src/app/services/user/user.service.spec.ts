e0d7f155970cd4a48313003e7ff87ec1cb507758
DESKTOP-VQQ442N\User <luca.albertario@liparipeople.com>
2025-06-11 10:53:03 +0200

    Risolto problema UserService con Karma

--- src/app/services/user/user.service.spec.ts
+++ src/app/services/user/user.service.spec.ts
@@ -1,16 +1,45 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UserService } from './user.service';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
-describe('UserService', () => {
+describe('UserService with manual HttpClient mock', () => {
   let service: UserService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    // crea un finto HttpClient con solo il metodo .get()
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        UserService,
+        { provide: HttpClient, useValue: httpClientSpy },
+      ]
+    });
+
     service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call HttpClient.get', () => {
+    const expectedData = {
+      success: true,
+      error: "",
+      data: [{
+        id: 1,
+        username: "Luca"
+      }]
+    }
+    httpClientSpy.get.and.returnValue(of(expectedData)); // restituisce un Observable
+
+    service.getUsers().subscribe(data => {
+      expect(data).toEqual(expectedData);
+    });
+
+    expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
   });
 });
