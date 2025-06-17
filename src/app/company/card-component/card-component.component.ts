c0a15535a03da7c65ec9065a3d2d1d1606e1dd9f
Carol04k <79216467+Carol04k@users.noreply.github.com>
2025-06-17 17:22:24 +0200

    s

--- src/app/company/card-component/card-component.component.ts
+++ src/app/company/card-component/card-component.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CompanyService } from '../../services/company/company.service';
+import { CompanyModel } from '../../model/Company';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-component.component.html',
   styleUrl: './card-component.component.css'
 })
 export class CardComponentComponent {
+  cardStats : CompanyModel[] = []
 
+  constructor(private companyService: CompanyService) {}
 }
