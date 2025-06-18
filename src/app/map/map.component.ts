98130979f3cc37fe662126903b967689ea668ea1
DESKTOP-VQQ442N\User <luca.albertario@liparipeople.com>
2025-06-18 10:18:58 +0200

    aggiunta ngClass e companyColors nel ts (importante)

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
 import { CardComponentComponent } from '../company/card-component/card-component.component';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-map',
-  imports: [CardComponentComponent],
+  imports: [CardComponentComponent, CommonModule],
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
-
-
-
 export class MapComponent {
+  companyColors : any = { //TODO aggiornamento colore con status back-end
+    "reply-lombardia" : "available",
+    "kmpg-lombardia" : "inStaffing",
+    "deloitte-lombardia" : "unavailable",
+    
 
-  isSelected(nodeId:String) {
+  } 
+  isSelected(nodeId: String) {
     document.getElementsByClassName("nodes");
-    alert("cerchio cliccato: " + nodeId );
+    alert("cerchio cliccato: " + nodeId);
     document.getElementById("card");
     // TODO prendere componente cardBox con Json?
 
