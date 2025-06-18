1f044a4db3526db26b14495e25b66c59226adcc2
DESKTOP-VQQ442N\User <luca.albertario@liparipeople.com>
2025-06-18 12:09:35 +0200

    setting companies in companyColor

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,13 +11,42 @@ import { CommonModule } from '@angular/common';
   styleUrl: './map.component.css'
 })
 export class MapComponent {
-  companyColors : any = { //TODO aggiornamento colore con status back-end
-    "reply-lombardia" : "available",
-    "kmpg-lombardia" : "inStaffing",
-    "deloitte-lombardia" : "unavailable",
-    
-
-  } 
+  
+companyColors: any = {
+  //TODO aggiornamento colore con status back-end
+  "deloitte-lombardia": "available",
+  "accenture-lombardia": "unavailable",
+  "ey-lombardia": "available",
+  "reply-lombardia": "inStaffing",
+  "pwc-lombardia": "available",
+  "kmpg-lombardia": "available",
+  "kmpg-piemonte": "unavailable",
+  "ey-trentino-alto-adige": "unavailable",
+  "accenture-trentino-alto-adige": "unavailable",
+  "pwc-valle-d-aosta": "unavailable",
+  "reply-friuli": "unavailable",
+  "be-consulting-friuli": "unavailable",
+  "accenture-liguria": "unavailable",
+  "be-consulting-sardegna": "unavailable",
+  "engineering-sardegna": "unavailable",
+  "deloitte-veneto": "unavailable",
+  "ey-veneto": "unavailable",
+  "ey-emilia": "unavailable",
+  "pwc-emilia": "unavailable",
+  "reply-toscana": "unavailable",
+  "pwc-lazio": "unavailable",
+  "ey-lazio": "unavailable",
+  "accenture-lazio": "unavailable",
+  "accenture-molise": "unavailable",
+  "deloitte-marche": "unavailable",
+  "accenture-umbria": "unavailable",
+  "reply-abruzzo": "unavailable",
+  "deloitte-campania": "unavailable",
+  "accenture-basilicata": "unavailable",
+  "deloitte-puglia": "unavailable",
+  "engineering-calabria": "unavailable",
+  "engineering-sicilia": "unavailable"
+}
   isSelected(nodeId: String) {
     document.getElementsByClassName("nodes");
     alert("cerchio cliccato: " + nodeId);
