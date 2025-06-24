aa6a415e91d88bb37c9a94b997eb276459358665
Carol04k <79216467+Carol04k@users.noreply.github.com>
2025-06-24 16:32:15 +0200

    modifche a table budget ts

--- src/app/table-budget/table-budget.component.ts
+++ src/app/table-budget/table-budget.component.ts
@@ -1,24 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TurnService } from '../services/turn/turn.service';
 import { Observable } from 'rxjs';
 import { Response } from 'express';
 import { HttpClient } from '@angular/common/http';
 import { TurnModel } from '../model/Turn';
+import { ActivatedRoute, Params } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-table-budget',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './table-budget.component.html',
   styleUrl: './table-budget.component.css'
 })
-export class TableBudgetComponent {
-  private getUrlTurn = 'http://localhost:8080/api/turn';
-  constructor(private turnService: TurnService) { }
-  createdTurn?: TurnModel;
-  createdBudget?: TurnModel;
+export class TableBudgetComponent implements OnInit{
+
+  currentTurn!: number
+  gameId!: number
+  
+  /* createdTurn?: TurnModel;
+  createdBudget?: TurnModel; */
+
+  constructor(private turnService: TurnService, private route: ActivatedRoute) { }
+
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      const idFromRoute = params['id'];
+      if (idFromRoute) {
+        this.gameId = +idFromRoute;
+        this.getTurns(this.gameId);
+      }
+    })
+  }
   
 
-
+  getTurns(gameId: number): void {
+    this.turnService.getTurns(gameId).subscribe({
+      next: (res) => {
+        console.log('turno corrente ', res);
+      },
+      error: (err) => {
+        console.error('errore nel turno corrente ', err);
+      }
+    })
+  }
 
 
   //TODO
